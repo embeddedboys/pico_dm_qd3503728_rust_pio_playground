f865c3daec328c39195dc7a328265447e9cd77ea
iotah <writeforever@foxmail.com>
2024-10-07 22:49:59 +0800

    add graphics fill_contiguous basiclly support.
    Signed-off-by: iotah <writeforever@foxmail.com>

--- src/graphics.rs
+++ src/graphics.rs
@@ -43,7 +43,26 @@ where
     where
         I: IntoIterator<Item = Self::Color>,
     {
-        info!("fill_contiguous");
+        let intersection = area.intersection(&self.bounding_box());
+        let Some(bottom_right) = intersection.bottom_right() else {
+            // No intersection -> nothing to draw
+            return Ok(());
+        };
+        let xs = area.top_left.x as u16;
+        let ys = area.top_left.y as u16;
+        let xe = bottom_right.x as u16;
+        let ye = bottom_right.y as u16;
+
+        let count = intersection.size.width * intersection.size.height;
+
+        let mut colors = colors.into_iter();
+        if &intersection == area {
+            // Draw the original iterator if no edge overlaps the framebuffer
+            self.set_addr_win(xs, ys, xe, ye)?;
+            self.write_pixels(colors)?;
+        } else {
+            info!("overlap not supports yet!");
+        }
         Ok(())
     }
 
